55f0b79ebedc093636e262a0932d4b54f95ffaf5
VijayGohel <vijaygohel5454@gmail.com>
2023-01-22 22:50:44 +0530

    auth using jwt

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { deleteUser, followUser, unfollowUser, getUser, updateUser, getAllUsers } from "../controllers/UserController.js";
+import authMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router  = express.Router();
 
 router.get("/:id", getUser)
 .get("/", getAllUsers)
-.patch("/:id", updateUser)
-.delete("/:id", deleteUser)
-.patch("/:id/follow", followUser)
-.patch("/:id/unfollow", unfollowUser);
+.patch("/:id", authMiddleware, updateUser)
+.delete("/:id", authMiddleware, deleteUser)
+.patch("/:id/follow", authMiddleware, followUser)
+.patch("/:id/unfollow", authMiddleware, unfollowUser);
 
 export default router;
